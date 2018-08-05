058aaf405da247ff0f15c5349d375c60078c734d
Jose Naranjo <naranjja@gmail.com>
2018-08-05 15:10:28 -0500

    :hankey: Add initial project logic

--- app/main-page.js
+++ app/main-page.js
@@ -1,41 +1,24 @@
-/*
-In NativeScript, a file with the same name as an XML file is known as
-a code-behind file. The code-behind is a great place to place your view
-logic, and to set up your page’s data binding.
-*/
+const LocalNotifications = require("nativescript-local-notifications")
+const power = require("nativescript-powerinfo")
+const dialogs = require("ui/dialogs")
 
-/*
-NativeScript adheres to the CommonJS specification for dealing with
-JavaScript modules. The CommonJS require() function is how you import
-JavaScript modules defined in other files.
-*/ 
-var createViewModel = require("./main-view-model").createViewModel;
+const getBatteryLevel = () => new Promise((resolve, reject) => {
+    power.startPowerUpdates((Info) => resolve(Info.percent))
+})
 
-function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    var page = args.object;
-
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = createViewModel();
+const notifyBatteryLevel = (batteryLevel) => {
+    LocalNotifications.schedule([{
+        title: `Your battery has reached ${batteryLevel}%`,
+        body: "Better charge it!",
+        at: new Date()
+      }])
 }
 
-/*
-Exporting a function in a NativeScript code-behind file makes it accessible
-to the file’s corresponding XML file. In this case, exporting the onNavigatingTo
-function here makes the navigatingTo="onNavigatingTo" binding in this page’s XML
-file work.
-*/
-exports.onNavigatingTo = onNavigatingTo;
+exports.onNavigatingTo = (args) => {
+    const page = args.object
+}
+
+exports.onNotificationTap = (args) => {
+    getBatteryLevel()
+        .then((batteryLevel) => notifyBatteryLevel(batteryLevel))
+}
